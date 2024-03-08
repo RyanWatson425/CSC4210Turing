94c80023bcfffdcb55917e94348acfddf5722bd0
RyanWatson425 <watsonr3@spu.edu>
2024-03-07 22:04:17 -0800

    fix: removed MetaData description to fix versioning issue

--- turing-machine/src/app/layout.tsx
+++ turing-machine/src/app/layout.tsx
@@ -1,13 +1,7 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Turing Machine",
-  description: "Developed by Ryan Watson for an extra credit assignment in CSC4230",
-};
 
 export default function RootLayout({
   children,
